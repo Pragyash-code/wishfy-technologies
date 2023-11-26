0c546d22307811aeeaa7e817d248c02f8fde0509
pragyash-matchmyflight <pragyash.mishra@matchmyflight.com>
2023-11-26 22:38:36 +0530

    fixs

--- src/main/java/com/application/entities/Employee.java
+++ src/main/java/com/application/entities/Employee.java
@@ -1,43 +1,30 @@
 package com.application.entities;
 
-import java.time.LocalDate;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "employee")
 public class Employee {
 
     @Id
-    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", length = 10)
     private int id;
 
-    @NotNull
     @Column(name = "name", length = 50)
     private String name;
 
-    @NotNull
     @Column(name = "department", length = 10)
     private String department;
 
-    @NotNull
     @Column(name = "email", length = 50)
     private String email;
 
-    @NotNull
-    @Column(name = "joiningDate", length = 50)
+    @Column(name = "joiningDate")
     private LocalDate joiningDate;
 
 }
